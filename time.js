fbf15a8284abb85e0cd7c05eb405979bdfed9bd6
Hannah <nguyen8427@gmail.com>
2017-06-30 11:26:22 -0400

    create a time.js file

---
+++ time.js
@@ -0,0 +1,12 @@
+
+var HOUR = 8;
+var MINUTE = 50;
+var PERIOD = "AM";
+
+if(HOUR== 8 && MINUTE > 30 && PERIOD=="AM" ){
+console.log("It is almost 9 am");
+}
+else
+{
+console.log("It's just after 7 in the evening");
+}
